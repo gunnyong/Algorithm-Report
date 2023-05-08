b7de671ee70cd6547a0f75bd1f9e7a45edebf6ee
gunnyong <132893202+gunnyong@users.noreply.github.com>
2023-05-08 22:03:18 +0900

    Update QuickSort.h

--- QuickSort/QuickSort.h
+++ QuickSort/QuickSort.h
@@ -2,16 +2,18 @@
 #ifndef QUICKSORT_H
 #define QUICKSORT_H
 
+//클래스 선언부
 class QuickSort
 {
 private:
-	int* arr;
-	int size;
+	//멤버 변수 선언
+	int* arr; //정수형 배열
+	int size; //배열 크기
 public:
-	QuickSort(int size);
-	~QuickSort();
-	void print();
-	void quickSort(int l, int r);
+	QuickSort(int size); //생성자
+	~QuickSort(); //소멸자
+	void print(); //배열 출력 함수
+	void quickSort(int l, int r); //퀵 정렬 함수
 };
 
 #endif // !QUICKSORT_H
